44bb9869d4b44bf391d2c529d627297a109e114f
Nathan Burgess <nathans.email.address@gmail.com>
2022-07-26 18:05:58 -0700

    formatting to pass test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cupTwoPic = document.getElementById('cup-two-pic');
 const cupThreePic = document.getElementById('cup-three-pic');
 const totalEl = document.getElementById('total-count');
 const correctEl = document.getElementById('correct-count');
-const incorrectEl = document.getElementById('incorrect-count')
+const incorrectEl = document.getElementById('incorrect-count');
 
 // let state
 let totalCount = 0;
@@ -18,40 +18,40 @@ const cups = [cupOnePic, cupTwoPic, cupThreePic];
 // set event listeners 
 cupOneButton.addEventListener('click', () => {
 
-  const correctCup = getRandomItem(cups);
-  handleGuess(cupOnePic, correctCup);
+    const correctCup = getRandomItem(cups);
+    handleGuess(cupOnePic, correctCup);
 })
 cupTwoButton.addEventListener('click', () => {
-  const correctCup = getRandomItem(cups);
-  handleGuess(cupTwoPic, correctCup);
+    const correctCup = getRandomItem(cups);
+    handleGuess(cupTwoPic, correctCup);
 })
 cupThreeButton.addEventListener('click', () => {
-  const correctCup = getRandomItem(cups);
-  handleGuess(cupThreePic, correctCup);
+    const correctCup = getRandomItem(cups);
+    handleGuess(cupThreePic, correctCup);
 
 })
 
 function resetStyles() {
-  cupOnePic.src = '/assets/cup.png'
-  cupTwoPic.src = '/assets/cup.png'
-  cupThreePic.src = '/assets/cup.png'
+    cupOnePic.src = '/assets/cup.png'
+    cupTwoPic.src = '/assets/cup.png'
+    cupThreePic.src = '/assets/cup.png'
 }
 
 function getRandomItem(arr) {
-  const itemNumber = Math.floor(Math.random() * arr.length);
-  return arr[itemNumber];
+    const itemNumber = Math.floor(Math.random() * arr.length);
+    return arr[itemNumber];
 }
 
 function handleGuess(userGuess, correctSpot) {
-  resetStyles();
-  correctSpot.src = '/assets/upside-down-cup.jpg';
-  totalCount++;
-  if (userGuess === correctSpot) {
-    correctCount++;
-  }
-  totalEl.textContent = totalCount;
-  correctEl.textContent = correctCount;
-  incorrectEl.textContent = totalCount - correctCount;
+    resetStyles();
+    correctSpot.src = '/assets/upside-down-cup.jpg';
+    totalCount++;
+    if (userGuess === correctSpot) {
+        correctCount++;
+    }
+    totalEl.textContent = totalCount;
+    correctEl.textContent = correctCount;
+    incorrectEl.textContent = totalCount - correctCount;
 }
 
   // get user input
